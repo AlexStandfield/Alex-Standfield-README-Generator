5f5c93f8a7046fb4248154fb2942c52563943bd6
Alex Standfield <AlexStandfield@gmail.com>
2022-08-03 21:17:01 -0600

    functionality working

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,164 @@
 // TODO: Include packages needed for this application
-import inquirer from 'inquirer';
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your projects title?',
+    'Please provide a description by answering the following questions.',
+    [
+        'What was your motivation?', 
+        'Why did you build this project?', 
+        'What problem does it solve?', 
+        'What did you learn?',
+        'What makes your project stand out?'
+    ],
+    'Please list out the installation instructions needed for your project.',
+    'Please provide instructions and an example for use.',
+    `Please list anyone you'd like to list as a collaborater in the credits section.`,
+    `Please select any licenses your application is covered under.`,
+    `Please provide your GitHub username to add a link to your GitHub account.`,
+    `Please provide your email address so you may be reached for any additional questions.`
+];
+const licenses = [
+    'GNU AFFERO GENERAL PUBLIC LICENSE',
+    'GNU GENERAL PUBLIC LICENSE',
+    'GNU LESSER GENERAL PUBLIC LICENSE',
+    'Mozilla Public License',
+    'Apache License',
+    'MIT License',
+    'Boost Software License',
+    'The Unlicense'
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./Develop/product/README.md', data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'README Created'
+            })
+        })
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            // Project Title
+            {
+                type: 'input',
+                name: 'title',
+                message: questions[0],
+                validate: projectTitleInput => {
+                    if(projectTitleInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a title for your project!');
+                        return false;
+                    }
+                }
+            },
+            // Project Description
+            {
+                type: 'confirm',
+                name: 'description',
+                message: questions[1]
+            },
+            // Project Description Questions
+            {
+                type: 'input',
+                name: 'descriptionQuestion1',
+                message: questions[2][0]
+            },
+            {
+                type: 'input',
+                name: 'descriptionQuestion2',
+                message: questions[2][1]
+            },
+            {
+                type: 'input',
+                name: 'descriptionQuestion3',
+                message: questions[2][2]
+            },
+            {
+                type: 'input',
+                name: 'descriptionQuestion4',
+                message: questions[2][3]
+            },
+            {
+                type: 'input',
+                name: 'descriptionQuestion5',
+                message: questions[2][4]
+            },
+            // Installation
+            {
+                type: 'input',
+                name: 'installation',
+                message: questions[3]
+            },
+            // Usage
+            {
+                type: 'input',
+                name: 'usage',
+                message: questions[4]
+            },
+            // Credits
+            {
+                type: 'input',
+                name: 'credits',
+                message: questions[5]
+            },
+            // License
+            {
+                type: 'checkbox',
+                name: 'license',
+                message: questions[6],
+                choices: [
+                    licenses[0],
+                    licenses[1],
+                    licenses[2],
+                    licenses[3],
+                    licenses[4],
+                    licenses[5],
+                    licenses[6],
+                    licenses[7]
+                ]
+            },
+            // GitHub Username
+            {
+                type: 'input',
+                name: 'github',
+                message: questions[7]
+            },
+            // Email Address
+            {
+                type: 'input',
+                name: 'email',
+                message: questions[8]
+            }
+        ])
+        .then(readmeData => {
+            generateMarkdown.renderLicenseBadge(readmeData.license);
+            generateMarkdown.renderLicenseLink(readmeData.license);
+            generateMarkdown.renderLicenseSection(readmeData.license);
+            console.log(readmeData.license);
+            return readmeData
+        })
+        .then(readmeData => {
+            return generateMarkdown.generateMarkdown(readmeData);
+        })
+        .then(readme => {
+            return writeToFile(readme);
+        })
+}
 
 // Function call to initialize app
 init();
---
+++ Develop/product/README.md
@@ -0,0 +1,49 @@
+# alksdfj
+
+## Description
+asd fasd fasdf sadf asdf
+
+![GNU AFFERO GENERAL PUBLIC LICENSE](https://img.shields.io/badge/license-GNU%20AGPLv3-green),![GNU GENERAL PUBLIC LICENSE](https://img.shields.io/badge/license-GNU%20GPLv3-green),![GNU LESSER GENERAL PUBLIC LICENSE](https://img.shields.io/badge/license-GNU%20LGPLv3-green),![Mozilla Public License](https://img.shields.io/badge/license-MPL--2.0-green)
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+asdf
+
+## Usage
+asdf
+
+## Credits
+asdf
+
+## License
+Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+      Everyone is permitted to copy and distribute verbatim copies
+      of this license document, but changing it is not allowed.,Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+      Everyone is permitted to copy and distribute verbatim copies
+      of this license document, but changing it is not allowed.,Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+      Everyone is permitted to copy and distribute verbatim copies
+      of this license document, but changing it is not allowed.,Each Contributor hereby grants You a world-wide, royalty-free,
+      non-exclusive license:
+      
+      (a) under intellectual property rights (other than patent or trademark)
+          Licensable by such Contributor to use, reproduce, make available,
+          modify, display, perform, distribute, and otherwise exploit its
+          Contributions, either on an unmodified basis, with Modifications, or
+          as part of a Larger Work; and
+      
+      (b) under Patent Claims of such Contributor to make, use, sell, offer
+          for sale, have made, import, and otherwise transfer either its
+          Contributions or its Contributor Version.
+https://spdx.org/licenses/AGPL-3.0-or-later.html,https://spdx.org/licenses/GPL-3.0-or-later.html,https://spdx.org/licenses/LGPL-3.0-or-later.html,https://spdx.org/licenses/MPL-2.0.html
+
+## GitHub
+asdf
+
+## Email
+asdf
